f823722c53532c5b36eb114b58f89f3957af76a6
Shaunak Kashyap <ycombinator@gmail.com>
2021-11-08 09:54:57 -0800

    init checkin

---
+++ deployment/tf/deployment.tf
@@ -0,0 +1,31 @@
+terraform {
+#   required_version = ">= 0.12.29"
+
+  required_providers {
+    ec = {
+      source  = "elastic/ec"
+#       version = "0.4.0"
+    }
+  }
+}
+
+data "ec_stack" "latest" {
+  version_regex = "latest"
+  region        = "gcp-us-west1"
+}
+
+# Create an Elastic Cloud deployment
+resource "ec_deployment" "example_minimal" {
+  # Optional name.
+  name = "my_example_deployment"
+
+  # Mandatory fields
+  region                 = "gcp-us-weat1"
+  version                = data.ec_stack.latest.version
+  deployment_template_id = "aws-io-optimized-v2"
+
+  # Use the deployment template defaults
+  elasticsearch {}
+
+  kibana {}
+}
