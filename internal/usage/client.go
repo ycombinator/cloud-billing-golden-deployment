3acd28aef53565d7efc238fd05029acaf13fe10f
Shaunak Kashyap <ycombinator@gmail.com>
2021-12-06 17:37:34 -0800

    Implement Snapshot API requests

--- internal/usage/client.go
+++ internal/usage/client.go
@@ -199,8 +199,80 @@ func (c *Connection) GetDataInterNodeGB(q Query) (float64, error) {
 }
 
 func (c *Connection) GetSnapshotStorageSizeGB(q Query) (float64, error) {
+	// TODO
 	return 0, nil
 }
 func (c *Connection) GetSnapshotAPIRequestsCount(q Query) (float64, error) {
-	return 0, nil
+	// Build the request body.
+	usageTypeFilter := map[string]interface{}{
+		"term": map[string]string{
+			"ece.usage.type": "storage_api",
+		},
+	}
+	filters := q.toElasticsearchFilters("ece.source.cluster", "@timestamp")
+	filters = append(filters, usageTypeFilter)
+
+	var buf bytes.Buffer
+	query := map[string]interface{}{
+		"query": map[string]interface{}{
+			"bool": map[string]interface{}{
+				"filter": filters,
+			},
+		},
+		"aggs": map[string]interface{}{
+			"total": map[string]interface{}{
+				"sum": map[string]string{
+					"field": "ece.usage.count",
+				},
+			},
+		},
+	}
+
+	if err := json.NewEncoder(&buf).Encode(query); err != nil {
+		return 0, fmt.Errorf("error encoding query: %w", err)
+	}
+
+	fmt.Println("snapshot api requests query:", buf.String())
+
+	// Perform the search request.
+	res, err := c.esClient.Search(
+		c.esClient.Search.WithContext(context.Background()),
+		c.esClient.Search.WithIndex("usage-v*"),
+		c.esClient.Search.WithBody(&buf),
+		c.esClient.Search.WithSize(1),
+	)
+	if err != nil {
+		return 0, fmt.Errorf("error getting response: %w", err)
+	}
+	defer res.Body.Close()
+
+	if res.IsError() {
+		var e map[string]interface{}
+		if err := json.NewDecoder(res.Body).Decode(&e); err != nil {
+			return 0, fmt.Errorf("error parsing the response body: %w", err)
+		} else {
+			errStr := fmt.Errorf("query error: status: [%s], type: [%s], reason: [%s]",
+				res.Status(),
+				e["error"].(map[string]interface{})["type"],
+				e["error"].(map[string]interface{})["reason"],
+			)
+			return 0, errStr
+		}
+	}
+
+	var r struct {
+		Aggregations struct {
+			Total struct {
+				Value float64 `json:"value"`
+			} `json:"total"`
+		} `json:"aggregations"`
+	}
+
+	fmt.Println("snapshot api requests response:", res.String())
+
+	if err := json.NewDecoder(res.Body).Decode(&r); err != nil {
+		return 0, fmt.Errorf("error parsing the response body: %w", err)
+	}
+
+	return r.Aggregations.Total.Value, nil
 }
